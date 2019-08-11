aefd56157e7950eea5f7101644f656eb2c30546c
Sambbhav Garg <500061353@stu.upes.ac.in>
2019-08-11 16:28:00 +0530

    added edge searching in graph through sets

--- C++/Graphs/Search-Set-Graph.cpp
+++ C++/Graphs/Search-Set-Graph.cpp
@@ -8,8 +8,8 @@ struct Graph{
 Graph * createGraph(int V){
   Graph * graph = new Graph;
   graph->V = V;
-  graph->adjList = new unordered_set<int>[V];
-  return graph
+  graph->adjList = new set<int, greater<int>>[V];
+  return graph;
 }
 
 void addEdge(Graph * graph, int src, int dest){
@@ -19,23 +19,39 @@ void addEdge(Graph * graph, int src, int dest){
 
 void printGraph(Graph * graph){
   for(int i=0; i < graph->V; ++i){
-    unordered_set<int> lst = graph->adjList[i];
-    cout<<endl<<"Adjacency list of Vertex "<<V<<":";
+    set<int, greater<int> > lst = graph->adjList[i];
+    cout<<endl<<"Adjacency list of Vertex "<<i<<":";
     for(auto itr=lst.begin(); itr != lst.end(); ++itr)
-      cout<<*itr<<"";
+      cout<<*itr<<" ";
     cout<<endl;
   }
 }
 
 void searchEdge(Graph * graph, int src, int dest){
   auto itr = graph->adjList[src].find(dest);
-  if(itr == graph.adjList[src].end())
-    cout<<"Edge from "<<src<<"to "<<dest<<" not found"<<endl;
-  els
-    cout<<"Edge from "<<src<<"to "<<dest<<" found"<<endl;
+  if(itr == graph->adjList[src].end())
+    cout<<"Edge from "<<src<<" to "<<dest<<" not found"<<endl;
+  else
+    cout<<"Edge from "<<src<<" to "<<dest<<" found"<<endl;
 }
 
 int main(){
   int V=5;
   struct Graph * graph = createGraph(V);
+
+  addEdge(graph, 0, 1);
+  addEdge(graph, 0, 4);
+  addEdge(graph, 1, 2);
+  addEdge(graph, 1, 3);
+  addEdge(graph, 1, 4);
+  addEdge(graph, 2, 3);
+  addEdge(graph, 3, 4);
+
+  printGraph(graph);
+
+  searchEdge(graph, 3,2);
+  searchEdge(graph, 4,1);
+  searchEdge(graph, 2,4);
+
+
 }
