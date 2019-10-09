e4dca61a90bf8d1692fb6054203d3bfad4ce2919
sambbhavgarg <500061353@stu.upes.ac.in>
2019-10-10 01:55:27 +0530

    Update

--- C-Lang/Minor/test_mysql.c
+++ C-Lang/Minor/test_mysql.c
@@ -4,76 +4,68 @@ MYSQL_ROW row;
 MYSQL *con;
 MYSQL_RES *res;
 int num_fields;
-/*
-int main(int argc, char **argv){
-  printf("MySQL client version: %s", mysql_get_client_info());
-  exit(0);
+int num_rows;
+char *server = "localhost";
+char *user = "Minor";
+char *password = "minor1";
+char *database = "RTable";
 
-  MYSQL *con = mysql_init(NULL);
-  if(con == NULL){
-    fprintf(stderr, "%s\n", mysql_error(con));
-    exit(1);
-  }
-  if(mysql_real_connect(con, "localhost", "root", "root_pswd", NULL, 0, NULL, 0) == NULL){
-    fprintf(stderr, "%s\n", mysql_error(con));
-    mysql_close(con);
-    exit(1);
-  }
-  if (mysql_query(con, "CREATE DATABASE RTable")){
-    fprintf(stderr, "%s\n", mysql_error(con));
-    mysql_close(con);
-    exit(1);
-  }
-  mysql_close(con);
-  exit(0);
-
-*/
 void finish_with_error(MYSQL *con){
   fprintf(stderr, "%s\n", mysql_error(con));
   mysql_close(con);
   exit(1);
 }
 
-/*
+// ENTER DB NAME TO BE ENTERED
+void createDatabase(char * DBName){
+  if(mysql_real_connect(con, server, user, password, NULL, 0, NULL, 0) == NULL){
+    finish_with_error(con);
+  }
+  if (mysql_query(con, "CREATE DATABASE RTable")){
+    finish_with_error(con);
+  }
+}
 
-int main(int argv, char **argc){
-  MYSQL *con = mysql_init(NULL);
-  if(con==NULL){
-    fprintf(stderr, "%s\n", mysql_error(con));
-    exit(1);
+// RECEIVE TABLE NAME & AND FIELDS TO BE ENTERED
+void createTable(char * TableName){
+  printf("If this table exists, it will be dropped. Do you accept? (y/n)\n");
+
+  char choice;
+  scanf("%c", &choice);
+
+  if(choice == 'y' || choice == 'Y'){
+    if(mysql_query(con, "DROP TABLE IF EXISTS DISTANCES")){
+        finish_with_error(con);
+    }
+
+    if (mysql_query(con, "CREATE TABLE DISTANCES(A FLOAT, B FLOAT, C FLOAT, D FLOAT)")){
+      finish_with_error(con);
+    }
   }
-  if( mysql_real_connect(con, "localhost", "Minor", "minor1", "RTable", 0, NULL, 0) == NULL){
-    finish_with_error(con);
+  else{
+    printf("Table already exists!");
   }
-  if(mysql_query(con, "DROP TABLE IF EXISTS DISTANCES, TIMES")){
-    finish_with_error(con);
-  }
-  if (mysql_query(con, "CREATE TABLE DISTANCES(A FLOAT, B FLOAT, C FLOAT, D FLOAT)")){
-    finish_with_error(con);
-  }
+}
+
+// RECEIVE VALUES TO BE ENTERED
+void insertValues(){
   if(mysql_query(con, "INSERT INTO DISTANCES VALUES(4.70, 5.75, 3.99, 4.12)")){
     finish_with_error(con);
   }
-  if(mysql_query(con, "INSERT INTO DISTANCES VALUES(4.70, 5.75, 3.99, 4.12)")){
-    finish_with_error(con);
-  }
-  if (mysql_query(con, "CREATE TABLE TIMES(A FLOAT, B FLOAT, C FLOAT, D FLOAT)")){
-    finish_with_error(con);
-  }
-  if(mysql_query(con, "INSERT INTO TIMES VALUES(4.70, 5.75, 3.99, 4.12)")){
-    finish_with_error(con);
-  }
-  */
+}
+
 int main(int argv, char** argc){
   con = mysql_init(NULL);
-
   if(con == NULL){
     fprintf(stderr, "mysql_init() failed\n");
     exit(1);
   }
-  if(mysql_real_connect(con, "localhost", "Minor", "minor1", "RTable", 0, NULL, 0)==NULL){
+  //CREATE DATABASE BEFORE next line.
+
+  if(mysql_real_connect(con, server, user, password, database, 0, NULL, 0)==NULL){
     finish_with_error(con);
   }
+
   if(mysql_query(con, "SELECT * FROM DISTANCES")){
     finish_with_error(con);
   }
@@ -82,11 +74,27 @@ int main(int argv, char** argc){
   if(res == NULL){
     finish_with_error(con);
   }
+
+  num_rows = mysql_num_rows(res);
   num_fields = mysql_num_fields(res); //num_fields = no. of columns
 
+  int mat[num_rows][num_fields];
+  int mat_i = 0;
+
   while(row = mysql_fetch_row(res)){
-    for(int i=0; i<num_fields; i++){
-      printf("%s ", row[i] ? row[i] : "NULL" );
+    for(int j=0; j<num_fields; j++){
+      mat[mat_i][j] = (int)row[j] ? row[j] : NULL;
+      printf("%s ", row[j] ? row[j] : "NULL");
+    }
+    mat_i++;
+    printf("\n");
+  }
+
+  printf("\n");
+
+  for(int i=0; i<num_rows; i++){
+    for(int j=0; j<num_fields; j++){
+      printf("%d ", mat[i][j]);
     }
     printf("\n");
   }
