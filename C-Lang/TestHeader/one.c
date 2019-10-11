faed086a591ed19711125fb21ea236d1aa9fda76
sambbhavgarg <500061353@stu.upes.ac.in>
2019-10-12 00:22:48 +0530

    Reorganized

---
+++ C-Lang/TestHeader/one.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "share.c"
+#include "two.c"
+
+int n;
+
+int main(){
+  printf("%d\n", n);
+  getme();
+}
---
+++ C-Lang/TestHeader/two.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void getme(){
+  printf("Two: %d\n", n);
+}
