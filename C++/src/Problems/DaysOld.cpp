929b328a20a899ab13ad5ce7f1b86189e1cac5b1
sambbhavgarg <500061353@stu.upes.ac.in>
2020-02-21 00:42:21 +0530

    Latest

--- C++/src/Problems/DaysOld.cpp
+++ C++/src/Problems/DaysOld.cpp
@@ -8,19 +8,69 @@
 
 using namespace std;
 
+int daysBetweenBefore(map<int, int> mp, int birthDay, int birthMonth){
+  auto it = mp.find(currentMonth);
+  int daysBetween = 0;
+  int daysInCurrentMonth = it->second - currentDay;
+  for(int i = currentMonth+1; i<birthMonth; i++){
+    it = mp.find(i);
+    daysBetween += it->second;
+  }
+  daysBetween = daysBetween + daysInCurrentMonth + birthDay;
+  return daysBetween;
+}
+
+int daysBetweenAfter(map<int, int> mp, int birthDay, int birthMonth){
+  auto it = mp.find(birthMonth);
+  int daysBetween = 0;
+  int daysInBirthMonth = it->second - birthDay;
+  for(int i = birthMonth+1; i<currentMonth; i++){
+    it = mp.find(i);
+    daysBetween += it->second;
+  }
+  daysBetween = daysBetween + daysInBirthMonth + currentDay;
+  return daysBetween;
+}
+
+int currentDateBefore(int birthDate, int birthMonth){
+  if(birthMonth > currentMonth)
+    return 1;
+  else if(birthMonth == currentMonth){
+    if(birthDate > currentDay)
+      return 1;
+    else if(birthDate < currentDay)
+      return 0;
+  }
+  return 0;
+}
+
+int isLeap(int year){
+  if(((year%4 == 0) && (year%100 != 0)) || (year%400 == 0))
+    return 1;
+  return 0;
+}
+
 int findLeap(int start, int end){
   int flag=0;
-  for(int i=start; i<=end; i++){
-    if(((i%4 == 0) && (i%100 != 0)) || (i%400 == 0))
-      flag++;
-  }
+  for(int i=start; i<=end; i++)
+    flag += isLeap(i);
   return flag;
 }
 
-int checkLeapCovered(int date, int month){}
+int checkLeapCovered(){
+  if(isLeap(currentYear)){
+    if(currentMonth>2)
+      return 1;
+    else if(currentMonth == 2){
+      if(currentDay == 29)
+        return 1;
+      return 0;
+    }
+    return 0;
+  }
+}
 
 void createMonthHashTable(map<int, int> months){
-
   months.insert({1,31});
   months.insert({2,28});
   months.insert({3,31});
@@ -36,34 +86,32 @@ void createMonthHashTable(map<int, int> months){
 }
 
 int main(){
-  int d, m, y, daysBetween = 0;
+  int birthDay, birthMonth, birthYear;
   map<int, int> mp;
-  // map<int, int>::iterator it;
   createMonthHashTable(mp);
   // cout<<"Enter your birthdate seperated by '-'\nExample: 02-01-1999";
   cout<<"Enter your birth year: ";
-  cin>>y;
+  cin>>birthYear;
   cout<<"Enter your birth month: ";
-  cin>>m;
+  cin>>birthMonth;
   cout<<"Enter the day you were born: ";
-  cin>>d;
+  cin>>birthDay;
 
-  int years = currentYear - y;
-  int daysToBeAddedForLeapYears = findLeap(y, currentYear);
+  int years = currentYear - birthYear;
+  years = years*365;
 
-  years = years*365 + daysToBeAddedForLeapYears;
+  int currentLeap = checkLeapCovered();
+  int addLeap = findLeap(birthYear, currentYear) + currentLeap;
 
-  auto it = mp.find(m);
-  int daysInBirthMonth = it->second - d;
-  for(int i = m+1; i<currentMonth; i++){
-    it = mp.find(i);
-    daysBetween += it->second;
-  }
-  daysBetween = daysBetween + daysInBirthMonth + currentDay + years;
-  // if(!checkLeapCovered())
-  //   daysBetween -= 1;
-  cout<<"You are: "<<daysBetween<<" days old on "
+  if(currentDay == birthDay && currentMonth == birthMonth)
+    years += addLeap;
+  else if(currentDateBefore(birthDay, birthMonth))
+    years -= daysBetweenBefore(mp, birthDay, birthMonth) + addLeap;
+  else
+    years += daysBetweenAfter(mp, birthDay, birthMonth) + addLeap;
+
+  cout<<"You are: "<<years<<" days old on "
     <<currentDay<<"/"
     <<currentMonth<<"/"
-    <<currentYear<<"/\n";
+    <<currentYear<<"\n";
 }
